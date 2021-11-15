2ceebc2e8d82a35089ff9d4a4290841389109d15
nguyenhuutri1620vn <nguyenhuutri1620vn@gmail.com>
2021-11-15 10:56:19 +0700

    [Exercise_2] Update varrible name

--- Exercise_2.rb
+++ Exercise_2.rb
@@ -1,41 +1,41 @@
 class Staff
     def check_int(int)
         while !int.is_a? (Integer)
-            print "Date type input is wrong. Please enter again: "
+            print "Data type input is wrong. Please enter again: "
             int = Integer(gets) rescue false 
         end
         return int
     end
     def check_string(str)
         while str !~ /\D/
-            print "Date type input is wrong. Please enter again: "
+            print "Data type input is wrong. Please enter again: "
             str = gets.chomp
         end
     end
     def call
         print "Input number: "
-        @_stt = Integer(gets) rescue false 
-        check_int(@_stt)
+        @stt = Integer(gets) rescue false 
+        check_int(@stt)
         print "Input code: "
-        @_id = gets.chomp
+        @id = gets.chomp
         print "Input name: "
-        @_name = gets.chomp
-        check_string(@_name)
+        @name = gets.chomp
+        check_string(@name)
         print "Input year of birth: "
-        @_year = Integer(gets) rescue false 
-        @_year = check_int(@_year)
+        @year = Integer(gets) rescue false 
+        @year = check_int(@year)
         print "Input position: "
-        @_position = gets.chomp
-        check_string(@_position)
+        @position = gets.chomp
+        check_string(@position)
         print "Input salary per day: "
-        @_salary = Integer(gets) rescue false
-        @_salary = check_int(@_salary)
+        @salary = Integer(gets) rescue false
+        @salary = check_int(@salary)
         print "Input day: "
-        @_timework = Integer(gets) rescue false
-        @_timework = check_int(@_timework)
+        @timework = Integer(gets) rescue false
+        @timework = check_int(@timework)
     end
     def to_s
-        puts "Code: #{@_id}, name : #{@_name}, age: #{2021 - @_year}, position: #{@_position.capitalize}, salary: #{@_salary * @_timework}"
+        puts "Code: #{@id}, name : #{@name}, age: #{2021 - @year}, position: #{@position.capitalize}, salary: #{@salary * @timework}"
     end
 end
 staff = Staff.new
