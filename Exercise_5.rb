417ffddffeb8c8af6fe44d743e548cbfed3d7dd8
nguyenhuutri1620vn <nguyehuutri1620vn@gmail.com>
2021-11-19 09:52:37 +0700

    [Exercise_5] Update condition (0<input<100) and add comment

--- Exercise_5.rb
+++ Exercise_5.rb
@@ -1,6 +1,8 @@
+#initialize varrible
 my_arrays= []
 pre = 0;
 count = 1;
+#check input
 def check(int)
     while !int.is_a?(Integer) || !int.between?(1,99)
         print "Data type input is wrong. Please enter again: "
@@ -8,6 +10,7 @@ def check(int)
     end
     return int
 end
+#input and create random array
 print "Input number element in your array (<100): "
 input = Integer(gets)rescue false 
 input =  check(input)
@@ -15,12 +18,14 @@ for i in 1..input
     random_number = rand(1..9)
     my_arrays << random_number
 end
+#show array
 puts "Random array: #{my_arrays.inspect}"
+#show child array
 for i in 0..input-1
     if i == input -1
-        puts "Array child increase [#{count}]: #{my_arrays[pre..i]}"
+        puts "Child array increase [#{count}]: #{my_arrays[pre..i]}"
     elsif my_arrays[i] >= my_arrays[i+1]
-        puts "Array child increase [#{count}]: #{my_arrays[pre..i]}"
+        puts "Child array increase  [#{count}]: #{my_arrays[pre..i]}"
         pre = i + 1
         count += 1
     end
