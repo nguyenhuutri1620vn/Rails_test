6297b82ac55a98d76280f263749bb10b618365bc
nguyenhuutri1620vn <nguyehuutri1620vn@gmail.com>
2021-11-27 13:51:13 +0700

    [Exercise_5] Update after Mr.Vu feedback

--- Exercise_5.rb
+++ Exercise_5.rb
@@ -1,5 +1,5 @@
 #initialize varrible
-my_arrays= []
+my_array= []
 pre = 0;
 count = 1;
 #check input
@@ -10,23 +10,31 @@ def check(int)
     end
     int
 end
-#input and create random array
-print "Input number element in your array (<100): "
-input = Integer(gets)rescue false 
-input =  check(input)
-for i in 0..input
-    random_number = rand(1..9)
-    my_arrays << random_number
-end
-#show array
-puts "Random array: #{my_arrays.inspect}"
-#show child array
-for i in 0..input
-    if i == input
-        puts "Child array increase [#{count}]: #{my_arrays[i]}"
-    elsif my_arrays[i] >= my_arrays[i+1]
-        puts "Child array increase [#{count}]: #{my_arrays[pre..i].to_s}"
-        pre = i + 1
-        count += 1
+
+def create_random_array(input, array = [])
+    for i in 0..input
+        random = rand(1..9)
+        array << random
     end
+end
+
+def show_child_array(input, count, my_array, pre)
+    for i in 0..input
+        if i == input
+            puts "Child array increase [#{count}]: #{my_array[i]}"
+        elsif my_array[i] >= my_array[i+1]
+            puts "Child array increase [#{count}]: #{my_array[pre..i].to_s}"
+            pre = i + 1
+            count += 1
+        end
+    end
+end
+
+begin
+    print "Input number element in your array (<100): "
+    input = Integer(gets)rescue false 
+    input =  check(input)
+    create_random_array(input, my_array)
+    puts "Random array: #{my_array.inspect}"
+    show_child_array(input, count, my_array, pre)
 end
