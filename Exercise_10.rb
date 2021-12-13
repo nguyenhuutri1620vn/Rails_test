a62c92850d11f2d70af1219d423e836b4308238e
nguyenhuutri1620vn <nguyehuutri1620vn@gmail.com>
2021-12-13 10:45:25 +0700

    [Exercise_10] Clean code

---
+++ Exercise_10.rb
@@ -0,0 +1,29 @@
+# handle array 
+def output_handle_elements_in_array_equal_target_sum(numbers = [], target_sum)
+    current = 0
+    count_with_current = 1
+    count = 1
+    output_numbers = []
+
+    while current < numbers.length && count < numbers.length
+        count += 1
+        if count == numbers.length
+            count_with_current += 1
+            count = count_with_current + 1
+            if count_with_current == numbers.length - 1
+                current += 1
+                count_with_current = current + 1
+                count = count_with_current + 1
+                break if current == numbers.length - 2
+            end
+        end
+        output_numbers << [numbers[current], numbers[count_with_current], numbers[count]] if target_sum == numbers[current] + numbers[count_with_current] + numbers[count]
+    end
+    output_numbers
+end
+
+#Initialize varrible
+numbers = [12, 5, 3, 2, -6, 5, -8, 6]
+target_sum = 0
+
+p output_handle_elements_in_array_equal_target_sum(numbers, target_sum)
